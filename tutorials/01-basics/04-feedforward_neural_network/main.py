cd6c25db3ffd3b1b7aa62277bae5dee89c53bb45
hezekiah243 <liuhan213@gmail.com>
2019-12-23 21:48:27 +1100

    compare linear model and nn model

--- tutorials/01-basics/04-feedforward_neural_network/main.py
+++ tutorials/01-basics/04-feedforward_neural_network/main.py
@@ -38,10 +38,11 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
+        # define each layer
         self.fc1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)  
-    
+        # build layers into structure.    
     def forward(self, x):
         out = self.fc1(x)
         out = self.relu(out)
